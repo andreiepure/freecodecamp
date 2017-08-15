0f7b30945fe7cfd70f49f16fc884d6dfdaf55169
Andrei Epure <epure.andrei@gmail.com>
2017-08-15 18:23:48 +0200

    finished the Wikipedia Viewer project

--- wikipedia/js/script.js
+++ wikipedia/js/script.js
@@ -1,54 +1,43 @@
-showHide = function() {
-	this.css = new function() {
-		this.HIDDEN = 'hidden';
-	}();
-
-	// show and hide for an HTML element
-	this.showElement = function(element) {
-		if (element) {
-			element.classList.remove(css.HIDDEN);
-		}
-	}();
-
-	this.hideElement = function(element) {
-		if (element) {
-			element.classList.add(css.HIDDEN);
-		}
-	}();
-
-	this.hideElements = function(elements) {
-		try {
-			for (var element in elements) {
-				if (elements.hasOwnProperty(element)) {
-					hideElement(elements[element]);
-				}
+var showHide = new function() {
+	this.operation = function(elements, op) {
+		for(var i = 0; i < elements.length; i++) {
+			var item = elements.item(i);
+			if (op === 'show') {
+				item.classList.remove('hidden');
+			}
+			else if (op === 'hide') {
+				item.classList.add('hidden');
 			}
 		}
-		catch (e) {
-			console.log('hideElements was given incorrect input');
-		}
-	}();
+	};
 }();
 
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function(event) { 
 
 	var searchInput = document.getElementById('search-input');
 	var searchButton = document.getElementById('search-button');
-	var urlTemplate = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&generator=search&exsentences=1&exlimit=max&exintro=1&explaintext=1&gsrlimit=8&gsrsearch=";
+	var urlTemplate = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&generator=search&exsentences=1&exlimit=max&exintro=1&explaintext=1&gsrlimit=8&origin=*&gsrsearch=";
+	var cards = document.getElementsByClassName('card');
 	var cardTitles = document.getElementsByClassName('card-title');
 	var cardTexts = document.getElementsByClassName('card-text');
+	var cardLinks = document.getElementsByClassName('card-link');
 
 	function setUi(response) {
-		hideElements(listItems);
-
-		for (var i = 0; i < 8; i++) {
-			var article = response.query.search[i];
+		var i = 0;
+		for (page in response.query.pages) {
+			var article = response.query.pages[page];
 			cardTitles[i].innerText = article.title;
 			cardTexts[i].innerHTML = article.extract;
+			cardLinks[i].href = "https://en.wikipedia.org/?curid=" + article.pageid;
+			i++;
 		}
+
+		showHide.operation(cards, 'show');
 	}
 
 	function doSearch() {
+		showHide.operation(cards, 'hide');
+
 		var searchText = encodeURIComponent(searchInput.value);
 		var url = urlTemplate + searchText;
 
